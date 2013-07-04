bc8440fa42332ccf314c9161e76d8ee74f10d117
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-07-04 14:34:16 +0300

    progress so far

--- populate.js
+++ populate.js
@@ -9,17 +9,46 @@ mongo.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
   collection.remove(function() {});
   
   var data = [
-        {id: 1, title: 'Place A', description: 'Lorem Ipsum bla bla bla', coords: [58.58,25.1], date: new moment().subtract('weeks', 2).format()},
-        {id: 2, title: 'Place B', description: 'Lorem Ipsum bla bla bla',  coords: [58.8,25.5], date: new moment().subtract('weeks', 2).format()},
-        {id: 3, title: 'Place D', description: 'Lorem Ipsum bla bla bla',  coords: [58.18,24.1], date: new moment().subtract('week', 1).format()},
-        {id: 4, title: 'Place E', description: 'Lorem Ipsum bla bla bla',  coords: [58.9,25.3], date: new moment().subtract('week', 1).format()},
-        {id: 5, title: 'Place F', description: 'Lorem Ipsum bla bla bla',  coords: [57.98,24.1], date: new moment().format()},
-        {id: 6, title: 'Place G', description: 'Lorem Ipsum bla bla bla',  coords: [57.96,25.3], date: new moment().format()}
+        {
+          id: 1, 
+          title: 'Place A', 
+          description: 'Lorem Ipsum bla bla bla', 
+          type: 'Water well', 
+          priority: 1, 
+          coords: [58.58,25.1], 
+          date: new moment().subtract('weeks', 2).format()
+        },
+        {
+          id: 2, 
+          title: 'Place B', 
+          description: 'Lorem Ipsum bla bla bla', 
+          type: 'Some pump', 
+          priority: 1, 
+          coords: [58.59,25.2], 
+          date: new moment().subtract('weeks', 1).format()
+        },
+        {
+          id: 3, 
+          title: 'Place C', 
+          description: 'Lorem Ipsum bla bla bla', 
+          type: 'New mine', 
+          priority: 2, 
+          coords: [58.58,25.4], 
+          date: new moment().format()
+        },
+        {
+          id: 4, 
+          title: 'Place D', 
+          description: 'Lorem Ipsum bla bla bla', 
+          type: 'New mine', 
+          priority: 1, 
+          coords: [58.62,25.7], 
+          date: new moment().format()
+        }
     ]
       
   collection.insert(data, {w:1}, function(err, result) {
     console.log(result)
-    db.close()
   });
 
 });
