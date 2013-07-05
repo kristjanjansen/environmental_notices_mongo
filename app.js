ede22e12a3aaf019d1b4bdd3bc8a4a3a5a23b181
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-07-05 07:54:44 +0300

    mongo fix

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ express = require('express')
 var mongo = require('mongodb').MongoClient;
 var moment = require('moment')
 
+mongo.connect(config.mongoUrl, function(err, db) {
+  if (err) throw err;
+  var collection = db.collection(config.mongoCollection);
+
 app = express()
 
 app.use(express.static(__dirname + '/public'))
@@ -12,9 +16,6 @@ app.get('/config', function(req, res){
 });
 
 app.get('/api/week/:week', function(req, res){
-  mongo.connect(config.mongoUrl, function(err, db) {
-    if (err) throw err;
-    var collection = db.collection(config.mongoCollection);
     var week = parseInt(req.params.week);
     var start = moment().week(week).startOf('week').add('days', 1).format()
     var end = moment().week(week).startOf('week').add('days', 8).format()
@@ -22,7 +23,6 @@ app.get('/api/week/:week', function(req, res){
     collection.find({date: {$gte: start, $lt: end}}).sort({priority: -1, type: 1 }).toArray(function(err, items) {
       res.send({markers: items});
     });  
-  })
 });
 
 app.get('/*', function(req, res){
@@ -30,3 +30,5 @@ app.get('/*', function(req, res){
 });
 
 app.listen(8001)
+
+})
