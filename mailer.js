73807a9a08fb1f9d8d9b3e708a98370493ffb5a6
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-07-07 18:08:49 +0300

    latest

--- mailer.js
+++ mailer.js
@@ -3,71 +3,75 @@ var nodemailer = require("nodemailer");
 var moment = require("moment");
 var mongo = require('mongodb').MongoClient;
 var hogan = require('hogan.js');
+var cron = require('cron').CronJob;
+   
+var job = new cron(config.mailCron, function() {
 
-
-var smtpTransport = nodemailer.createTransport("SMTP",{
-    service: "Gmail",
-    auth: {
-        user: config.mailUsername,
-        pass: config.mailPassword
-    }
-});
-
-  var tmpl = '\
-{{#items}}\
-<b>{{type}}</b><br />\
-{{date}}<br />\
-http://keskkonnateated.ee/{{week}}/{{id}}<br />\
-<br />\
-{{{description}}}<br />\
-<br />\
-Allikas: {{url}}<br />\
-<br />\
-<br />\
-{{/items}}\
----<br />\
-<br />\
-Keskkonnateated<br />\
-http://keskkonnateated.ee<br />\
-keskkonnateated@gmail.com\
-'
-
-mongo.connect(config.mongoUrl, function(err, db) {
-  if (err) throw err;
-  var collection = db.collection(config.mongoCollection);
-
-  var week = moment().subtract('weeks', 1).week()
-  var start = moment().week(week).startOf('week').add('days', 1)
-  var end = moment().week(week).startOf('week').add('days', 8)
-    
-  collection.find({date: {$gte: start.format(), $lt: end.format()}, priority: {$ne: 0}}).sort({priority: -1, type: 1 }).toArray(function(err, items) {
-    if (err) throw err;
-    
-    items = items.map(function(item) {
-      item.date = moment(item.date).format('DD.MM.YYYY')
-      item.week = week
-      return item
-    })
-    
-    var mailOptions = {
-      from: config.mailFrom,
-      to: config.mailTo, 
-      subject: 'Test2: Keskkonnateated ' + start.format('DD.MM.YYYY') + ' - ' + end.subtract('days', 1).format('DD.MM.YYYY'),
-      html: hogan.compile(tmpl).render({items: items})
-    }
-  
-  smtpTransport.sendMail(mailOptions, function(err, res) {
-      if (err) {
-        throw err
-      } else {
-        console.log("Message sent: " + res.message);
+  var smtpTransport = nodemailer.createTransport("SMTP",{
+      service: "Gmail",
+      auth: {
+          user: config.mailUsername,
+          pass: config.mailPassword
       }
-      smtpTransport.close();
-      db.close()
+  });
+
+    var tmpl = '\
+  {{#items}}\
+  <b>{{type}}</b><br />\
+  {{date}}<br />\
+  http://keskkonnateated.ee/{{week}}/{{id}}<br />\
+  <br />\
+  {{{description}}}<br />\
+  <br />\
+  Allikas: {{url}}<br />\
+  <br />\
+  <br />\
+  {{/items}}\
+  ---<br />\
+  <br />\
+  Keskkonnateated<br />\
+  http://keskkonnateated.ee<br />\
+  keskkonnateated@gmail.com\
+  '
+
+  mongo.connect(config.mongoUrl, function(err, db) {
+    if (err) throw err;
+    var collection = db.collection(config.mongoCollection);
+
+    var week = moment().subtract('weeks', 1).week()
+    var start = moment().week(week).startOf('week').add('days', 1)
+    var end = moment().week(week).startOf('week').add('days', 8)
+    
+    collection.find({date: {$gte: start.format(), $lt: end.format()}, priority: {$ne: 0}}).sort({priority: -1, type: 1 }).toArray(function(err, items) {
+      if (err) throw err;
+    
+      items = items.map(function(item) {
+        item.date = moment(item.date).format('DD.MM.YYYY')
+        item.week = week
+        return item
+      })
+    
+      var mailOptions = {
+        from: config.mailFrom,
+        to: config.mailTo, 
+        subject: 'Test2: Keskkonnateated ' + start.format('DD.MM.YYYY') + ' - ' + end.subtract('days', 1).format('DD.MM.YYYY'),
+        html: hogan.compile(tmpl).render({items: items})
+      }
+  
+    smtpTransport.sendMail(mailOptions, function(err, res) {
+        if (err) {
+          throw err
+        } else {
+          console.log(moment().format() + ' Completed mailer: ' + res.message);
+        }
+        smtpTransport.close();
+        db.close()
       
+    });
+  
+
+    })
+  
   });
   
-
-  })
-  
-});
+}).start();
--- scraper.js
+++ scraper.js
@@ -3,41 +3,45 @@ var request = require('request')
 var each = require('each')
 var moment = require('moment')
 var mongo = require('mongodb').MongoClient
+var cron = require('cron').CronJob;
 
 var types = require('./lib/types')
 var processors = require('./lib/processors')
   
+var job = new cron(config.scrapeCron, function() {
 
-// Build URL array
+  console.log(moment().format() + ' Started scraper');
 
-var start = (((config.pageStart || 1) - 1) * 10) + 1
-var stop = (((config.pageCount || 1) - 1) * 10) + 11 + (start - 1)
+  var start = (((config.pageStart || 1) - 1) * 10) + 1
+  var stop = (((config.pageCount || 1) - 1) * 10) + 11 + (start - 1)
 
-var urls = []
+  var urls = []
 
-for (var i = start; i < stop; i = i + 10) {
-  urls.push('http://www.ametlikudteadaanded.ee/index.php?act=1&salguskpvavald=' + moment().subtract('M', 1).format('DD.MM.YYYY') + '&sloppkpvavald=' + moment().format('DD.MM.YYYY') + '&steateliigid=' + types.ids() + '&srange=' + i + '-' + (i + 9));
-}
+  for (var i = start; i < stop; i = i + 10) {
+    urls.push('http://www.ametlikudteadaanded.ee/index.php?act=1&salguskpvavald=' + moment().subtract('M', 1).format('DD.MM.YYYY') + '&sloppkpvavald=' + moment().format('DD.MM.YYYY') + '&steateliigid=' + types.ids() + '&srange=' + i + '-' + (i + 9));
+  }
 
-mongo.connect(config.mongoUrl, function(err, db) {
+  mongo.connect(config.mongoUrl, function(err, db) {
   
-  if (err) { throw err }
-  var collection = db.collection(config.mongoCollection);
-  collection.remove(function() {});
+    if (err) { throw err }
+    var collection = db.collection(config.mongoCollection);
+    collection.remove(function() {});
 
-  each(urls)
-  .on('item', function(url, i, next) {
-    request({url: url, encoding: 'binary'}, function (e, r, body) {
-      processors.processPage(collection, body, function() {
-        setTimeout(next, 0)
-      })
-    })  
-  })
-  .on('end', function() {
-    console.log('Scraper finished')
-    db.close()
+    each(urls)
+    .on('item', function(url, i, next) {
+      request({url: url, encoding: 'binary'}, function (e, r, body) {
+        processors.processPage(collection, body, function() {
+          setTimeout(next, 0)
+        })
+      })  
+    })
+    .on('end', function() {
+      console.log(moment().format() + ' Completed scraper')
+      db.close()
 
+    });
+    
   });
 
-    
-});
+}).start();
+
